adf97915775bd56840e32d945085d9ccad75ec11
EdwardWoodhub <edu.world@hotmail.com>
2026-08-31 21:14:26 +0800

    Change hostname from 'myNixVMxfce' to 'vmNixX11'

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,7 @@
   boot.loader.grub.useOSProber = true;
 
 # networking.hostName = "nixos"; # Define your hostname.
-  networking.hostName = "myNixVMxfce"; # Define your hostname.
+  networking.hostName = "vmNixX11"; # Define your hostname.
 # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
 # Configure network proxy if necessary
