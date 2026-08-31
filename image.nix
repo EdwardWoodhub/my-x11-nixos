6c326ec3368ade7eb224f2744678eff8e5270934
EdwardWoodhub <edu.world@hotmail.com>
2026-08-31 20:25:38 +0800

    Add Nix expression for Docker layered image

---
+++ image.nix
@@ -0,0 +1,28 @@
+{ pkgs ? import <nixpkgs> {}
+, configFile ? ./configuration.nix
+}:
+
+let
+  # 经典模式直接求值 NixOS 系统
+  nixos = import <nixpkgs/nixos> {
+    configuration = configFile;
+    system = "x86_64-linux"; # 如果是 ARM 机器则写 "aarch64-linux"
+  };
+
+  toplevel = nixos.system;
+in
+pkgs.dockerTools.buildLayeredImage {
+  name = "nixos-system-update";
+  tag = "latest";
+
+  # 最多切分 100 层，最大化复用基础库和未变动组件
+  maxLayers = 100;
+
+  # 将顶层闭包放入容器内部
+  contents = [ toplevel ];
+
+  config = {
+    Env = [ "TARGET_TOPLEVEL=${toplevel}" ];
+    Cmd = [ "${pkgs.coreutils}/bin/echo" "${toplevel}" ];
+  };
+}
